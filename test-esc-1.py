f27d99bfefcf31da58a0ec1b78a8598d945045a5
VAnDm <klirens32@gmail.com>
2024-10-05 13:30:41 +0300

    added draw_romb

--- test-esc-1.py
+++ test-esc-1.py
@@ -6,12 +6,49 @@ END = "\x1b[0m"
 CLEAR = "\033[H"
 
 
-def draw_line(offset = 0, length = 1, color = 222):
+def draw_line(offset=0, length=1, color=222):
     line = " " * length
     print(f"{' ' * offset}{SET_COLOR}{color}m{line}{END}")
 
 
-for i in range(20):
-    draw_line(length = 20, color = 47, offset = i)
-    print(f"{CLEAR}")
-    time.sleep(0.5)
+def draw_romb():
+    size = 15
+    center = size // 2
+    offset = size // 2
+
+    step = 1
+    length = 1
+
+    #print(size, center, offset)
+
+    colors = [222, 157]
+
+    while True:
+        for color in colors:
+            for line in range(size):
+                draw_line(offset, length, color)
+
+                if line < center:
+                    offset -= step
+                    length += step * 2
+                else:
+                    offset += step
+                    length -= step * 2
+            time.sleep(2)
+            print(f"\x1b[{size + 2}A")
+            time.sleep(2)
+            print(f"\x1b[{offset}D")
+
+
+            length = 1
+            offset = size // 2
+
+            time.sleep(1)
+
+
+draw_romb()
+#if __name__ == "__main__":
+    #for i in range(20):
+        #draw_line(length=20, color=47, offset=i)
+        #print(f"{CLEAR}")
+        #time.sleep(0.5)
